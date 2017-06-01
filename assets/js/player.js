96f62edbf01cc545de16191c2a93f491d54fc1e6
Sameer Mehra <sammysignal@gmail.com>
2017-06-01 13:47:29 -0400

    still dumb, but at least it runs slowly

--- assets/js/player.js
+++ assets/js/player.js
@@ -2,6 +2,11 @@
 var board,
 game = new Chess();
 
+var pieces = ['P', 'N', 'B', 'R', 'Q']
+var piecesB = ['p', 'n', 'b', 'r', 'q']
+
+var values = {'P': 1, 'N': 3, 'B': 3, 'R': 5, 'Q': 9}
+
 // do not pick up pieces if the game is over
 // only pick up pieces for White
 var onDragStart = function(source, piece, position, orientation) {
@@ -11,13 +16,15 @@ var onDragStart = function(source, piece, position, orientation) {
   }
 };
 
-var makeRandomMove = function() {
+var makeAPIMove = function() {
   var possibleMoves = game.moves();
+  console.log(game.fen())
 
   // game over
   if (possibleMoves.length === 0) return;
   $.post(
-    "http://sammysignal.pythonanywhere.com/chess/",
+    //"http://sammysignal.pythonanywhere.com/chess/",
+    "http://localhost:5000/chess/",
     {
       "fen": game.fen(),
       "possibleMoves": possibleMoves.toString()
@@ -26,6 +33,7 @@ var makeRandomMove = function() {
     function(data,status,xhr){
       game.move(data);
       board.position(game.fen());
+      console.log(game.fen())
   });
 
 
@@ -40,6 +48,84 @@ var makeRandomMove = function() {
   
 };
 
+var makeRandomMove = function() {
+  var possibleMoves = game.moves();
+  var randomIndex = Math.floor(Math.random() * possibleMoves.length);
+  game.move(possibleMoves[randomIndex]);
+}
+
+var countPieces = function(fen, turn) {
+  var p;
+  if (turn == 'b') {
+    p = pieces;
+  } else {
+    p = piecesB;
+  }
+  var counter = 0
+  for (var i = 0; i < p.length; i++) {
+    for (var j = 0; j < fen.length; j++) {
+      if (fen[j] == p[i]) { 
+        counter += values[p[i].toUpperCase()];
+      }
+    }
+  }
+  return (counter / (8+10+6+6+9));
+}
+
+var heuristic = function() {
+  var possibleMoves = game.moves();
+  var fen = game.fen().split(' ');
+  // fen string
+  var f = fen[0];
+  // whose turn, either 'b' or 'w;
+  var turn = fen[1];
+  // any subset of KQkq, representing black or white castling king or queen side.
+  // White is uppercase.
+  var canCastle = fen[2];
+  // en passant square, if opportunity exists.
+  var ep = fen[3];
+  // counts half moves to check for three-move repetition (resulting in tie)
+  var plies = fen[4];
+  var moveCounter = fen[5];
+
+  return countPieces(f, turn);
+}
+
+var makeIntelligentMove = function(depth) {
+  if (depth <= 0) {
+    makeRandomMove();
+    return;
+  }
+  var possibleMoves = game.moves();
+  var fen = game.fen().split(' ');
+  // fen string
+  var f = fen[0];
+  // whose turn, either 'b' or 'w;
+  var turn = fen[1];
+
+  var bestHeuristic = 0;
+  var bestMove;
+  for (var m=0; m < possibleMoves.length; m++) {
+    // make hypothetical move
+    game.move(possibleMoves[m]);
+    // let other player make move
+    makeIntelligentMove(depth-1);
+    // see the value of this state
+    var testH = heuristic();
+    if (testH > bestHeuristic) {
+      console.log(testH);
+      bestHeuristic = testH;
+      bestMove = possibleMoves[m];
+    }
+    game.undo();
+    game.undo();
+  }
+
+  // Make the move!
+  game.move(bestMove);
+  board.position(game.fen());
+}
+
 var onDrop = function(source, target) {
   // see if the move is legal
   var move = game.move({
@@ -52,7 +138,10 @@ var onDrop = function(source, target) {
   if (move === null) return 'snapback';
 
   // make random legal move for black
-  window.setTimeout(makeRandomMove, 250);
+  window.setTimeout(function(){
+    //makeRandomMove();
+    makeIntelligentMove(2);
+  }, 250);
 };
 
 // update the board position after the piece snap
